055a8e5f727da6509c2b68286e7928f9ca563e2e
xlassix <awhy14539@gmail.com>
2025-01-16 00:49:08 +0100

    Update package name to "VerityMoveOracles" in Move.toml

--- orchestrator/src/indexer/rooch.ts
+++ orchestrator/src/indexer/rooch.ts
@@ -254,6 +254,7 @@ export default class RoochIndexer extends Indexer {
         const tx = new Transaction();
         tx.callFunction({
           target: data.notify ?? "",
+          maxGas: 10_000,
         });
 
         const receipt = await client.signAndExecuteTransaction({
